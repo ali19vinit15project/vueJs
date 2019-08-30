07be7f3b88ec24509b6de2b51d7a595c259f2cfe
Asgar25 <asgara75@gmail.com>
2019-08-30 23:59:11 +0530

    upload first commit

---
+++ src/layout/dashboard/DashboardLayout.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="wrapper">
+    <side-bar>
+      <template slot="links">
+        <sidebar-link to="/dashboard" name="Dashboard" icon="ti-panel"/>
+        <sidebar-link to="/stats" name="User Profile" icon="ti-user"/>
+        <sidebar-link to="/table-list" name="Table List" icon="ti-view-list-alt"/>
+        <sidebar-link to="/typography" name="Typography" icon="ti-text"/>
+        <sidebar-link to="/icons" name="Icons" icon="ti-pencil-alt2"/>
+        <sidebar-link to="/maps" name="Map" icon="ti-map"/>
+        <sidebar-link to="/notifications" name="Notifications" icon="ti-bell"/>
+      </template>
+      <mobile-menu>
+        <li class="nav-item">
+          <a class="nav-link">
+            <i class="ti-panel"></i>
+            <p>Stats</p>
+          </a>
+        </li>
+        <drop-down class="nav-item"
+                   title="5 Notifications"
+                   title-classes="nav-link"
+                   icon="ti-bell">
+          <a class="dropdown-item">Notification 1</a>
+          <a class="dropdown-item">Notification 2</a>
+          <a class="dropdown-item">Notification 3</a>
+          <a class="dropdown-item">Notification 4</a>
+          <a class="dropdown-item">Another notification</a>
+        </drop-down>
+        <li class="nav-item">
+          <a class="nav-link">
+            <i class="ti-settings"></i>
+            <p>Settings</p>
+          </a>
+        </li>
+        <li class="divider"></li>
+      </mobile-menu>
+    </side-bar>
+    <div class="main-panel">
+      <top-navbar></top-navbar>
+
+      <h1>Click</h1>
+  <button @click="apicall">Click ME</button>
+  <span class="response">Helo</span>
+<!-- 
+      <dashboard-content @click.native="toggleSidebar">
+
+      </dashboard-content> -->
+
+      <content-footer></content-footer>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+</style>
+<script>
+import TopNavbar from "./TopNavbar.vue";
+import ContentFooter from "./ContentFooter.vue";
+import DashboardContent from "./Content.vue";
+import MobileMenu from "./MobileMenu";
+import axios from 'axios';
+
+export default {
+  components: {
+    TopNavbar,
+    ContentFooter,
+    DashboardContent,
+    MobileMenu
+  },
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+  /* created() {
+    axios.get(`http://ec2-13-233-108-190.ap-south-1.compute.amazonaws.com:8080/inventoryManagement/api/v1/healthCheck`)
+    .then(response => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }, */
+  methods: {
+
+    apicall() {
+    axios.get(`http://ec2-13-233-108-190.ap-south-1.compute.amazonaws.com:8080/inventoryManagement/api/v1/healthCheck`)
+    .then(response => {
+      this.posts = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+
+    },
+  toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
+      }
+    }
+  }
+};
+</script>
